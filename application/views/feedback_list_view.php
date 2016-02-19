c2d601db46669bc3a37581b9ae4aa367a67d301e
Littie <polik.palam@gmail.com>
2016-02-19 13:56:23 +0200

    [core] check encoding

--- application/views/feedback_list_view.php
+++ application/views/feedback_list_view.php
@@ -1,6 +1,23 @@
+<h2>Feedback list</h2>
+
 <?php
 
 foreach ($data as $elements) {
-    echo $elements['message'] . ' ';
-    echo $elements['time'] . "<br>";
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-md-6">
+                <div class="row">
+                    <?= $elements['message'] ?>
+                </div>
+                <div class="row">
+                    <?= $elements['time'] ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
 }
+?>
