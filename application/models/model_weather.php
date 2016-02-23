3f6bba07c10426c4e1d5bd0d8a3e952e278055e0
Littie <polik.palam@gmail.com>
2016-02-23 03:40:59 +0200

    [core] add feedback

--- application/models/model_weather.php
+++ application/models/model_weather.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'simple_html_dom.php';
+require '/util/simple_html_dom.php';
 
 define('ZP', 'http://gismeteo.ua/city/daily/5093/');
 
@@ -14,7 +14,7 @@ class Model_Weather extends Model
 
         $header = $html->find('.header')[0]->plaintext;                // Погода
         $city = $html->find('.typeM')[0]->plaintext;                   // Город
-        $temp = $html->find('.temp dd')[0]->plaintext;                    // Темпертура
+        $temp = $html->find('.temp dd')[0]->plaintext;                 // Темпертура
         $cloudness = $html->find('.cloudness td')[0]->plaintext;       // Облачность
         $wind = $html->find('.wind dd')[0]->plaintext;                 // Ветер
         $bar = $html->find('div[title=Давление] dd')[0]->plaintext;    // Давление
