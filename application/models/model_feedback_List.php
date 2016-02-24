b18387743b638c7960c03388d32f91d9cec42776
Littie <polik.palam@gmail.com>
2016-02-24 15:56:04 +0200

    [test] test commit

--- application/models/model_feedback_List.php
+++ application/models/model_feedback_List.php
@@ -22,7 +22,8 @@ class Model_Feedback_List extends Model
         {
             $set[$i++] = $row;
         }
-        //k;lfjalkjasf;alksj
+
+
         $statement = $this->dbConnection->query('select guest_users.name, feedback.message, feedback.time from guest_users, feedback where guest_users.id = feedback.guest_id ORDER BY time');
 
         $statement->setFetchMode(PDO::FETCH_ASSOC);
