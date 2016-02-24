776048bd7166b418c2617ad74729d38c78c2fd7b
Littie <polik.palam@gmail.com>
2016-02-24 15:51:39 +0200

    [db] add sort by time

--- application/models/model_feedback.php
+++ application/models/model_feedback.php
@@ -11,7 +11,7 @@ class Model_Feedback extends Model
     }
 
     function sendFeedback($name, $message) {
-        $statement = $this->dbConnection->prepare('INSERT INTO feedback (user_id, message, time) SELECT id, ?, ? FROM users WHERE name = ?');
+        $statement = $this->dbConnection->prepare('INSERT INTO feedback (user_id, message, time) SELECT id, ?, ? FROM users WHERE name = ? ORDER BY time');
         $statement->execute(array($message, date("Y-m-d H:i:s"), $name));
     }
 
@@ -19,7 +19,7 @@ class Model_Feedback extends Model
         $statement = $this->dbConnection->prepare('INSERT INTO guest_users (name, email) VALUES (?, ?)');
         $statement->execute(array($name, $email));
 
-        $statement = $this->dbConnection->prepare('INSERT INTO feedback (guest_id, message, time) SELECT id, ?, ? FROM guest_users WHERE name = ?');
+        $statement = $this->dbConnection->prepare('INSERT INTO feedback (guest_id, message, time) SELECT id, ?, ? FROM guest_users WHERE name = ? ORDER BY time');
         $statement->execute(array($message, date("Y-m-d H:i:s"), $name));
     }
 }
